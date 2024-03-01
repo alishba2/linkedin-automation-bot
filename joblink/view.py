aba08dc7c132a4702b5b01a8316dd6606e124b75
alishba2 <alishbashazadi79@gmail.com>
2024-03-01 10:57:36 +0500

    updated url

--- joblink/view.py
+++ joblink/view.py
@@ -168,17 +168,23 @@ def linkJobApply(driver):
             url = url + "&start=" + str(currentPageJobs)
             driver.get(url)
             time.sleep(random.uniform(1, constants.botSpeed))
-
-            offersPerPage = driver.find_elements(
-                By.XPATH, '//li[@data-occludable-job-id]')
-            offerIds = [(offer.get_attribute(
-                "data-occludable-job-id").split(":")[-1]) for offer in offersPerPage]
-            time.sleep(random.uniform(1, constants.botSpeed))
+            offersPerPage = driver.find_elements(By.XPATH, '//li[@data-occludable-job-id]')
+            offerIds = []
 
             for offer in offersPerPage:
-                if not element_exists(offer, By.XPATH, ".//*[contains(text(), 'Applied')]"):
-                    offerId = offer.get_attribute("data-occludable-job-id")
-                    offerIds.append(int(offerId.split(":")[-1]))
+                offer_id_attribute = offer.get_attribute("data-occludable-job-id")
+
+                if offer_id_attribute is not None:
+                    # Check if the attribute is not None before attempting to split
+                    offer_ids_split = offer_id_attribute.split(":")
+
+                    if len(offer_ids_split) >= 2:
+                        # Check if there's at least one element after splitting
+                        offer_id = offer_ids_split[-1]
+
+                        # Check if the element contains the text 'Applied'
+                        if not element_exists(offer, By.XPATH, ".//*[contains(text(), 'Applied')]"):
+                            offerIds.append(int(offer_id))
 
             for jobID in offerIds:
                 offerPage = 'https://www.linkedin.com/jobs/view/' + str(jobID)
